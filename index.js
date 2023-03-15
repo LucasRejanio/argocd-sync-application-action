60cf2e330a9e927ca0fd0ab167bc8fb9646d845c
Lucas Rejanio Alves De Menezes <lucas.menezes@willbank.com.br>
2023-03-15 10:03:31 -0300

    refactor: adjusting index.js to correct 'TypeError: error is not a function'

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const configurator = require("./src/utils/configurator")
 const argocd = require("./src/argocd")
 
 const getArgocdClientSecret = async () => {
-    try {
-        const secretName = `will-${process.env.ENVIRONMET_PREFIX}-foundation-platforms-secret-argocd-admin`;
-        const secretString = await secretManager.getSecret(secretName, process.env.AWS_REGION);
-        var secret = JSON.parse(secretString);
+    const secretName = `will-${process.env.ENVIRONMET_PREFIX}-foundation-platforms-secret-argocd-admin`;
+    const secretString = await secretManager.getSecret(secretName, process.env.AWS_REGION);
+    var secret = JSON.parse(secretString);
 
-        return secret['configs.secret.argocdServerAdminPassword'];
-    } catch (error) {
-        throw error(error);
-    }
+    return secret['configs.secret.argocdServerAdminPassword'];
 };
 
 const main = async () => {
@@ -21,15 +17,11 @@ const main = async () => {
     const argocdHost = core.getInput("argocd-host").toString();
     const argocdApplication = core.getInput("argocd-application").toString();
 
-    try {
-        await configurator.checkActionInputs(platformsToken, argocdHost, argocdApplication)
-        const argocdClientSecret = await getArgocdClientSecret();
-        const argocdSessionToken = await argocd.openSession(argocdClientSecret);
-
-        argocd.syncApplication(argocdSessionToken, process.env.APPLICATION_NAME)
-    } catch(error){
-        throw error(error);
-    }
+    await configurator.checkActionInputs(platformsToken, argocdHost, argocdApplication)
+    const argocdClientSecret = await getArgocdClientSecret();
+    const argocdSessionToken = await argocd.openSession(argocdClientSecret);
+    
+    argocd.syncApplication(argocdSessionToken, process.env.APPLICATION_NAME)
 };
 
 main();
