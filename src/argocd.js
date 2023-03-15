5d31f5993b6af1dc3fa12b8c3cb037b62497540f
Lucas Rejanio Alves De Menezes <lucas.menezes@willbank.com.br>
2023-03-15 11:19:30 -0300

    fix: rollback promise in openSession

--- src/argocd.js
+++ src/argocd.js
@@ -14,35 +14,21 @@ const openSession = async (argocdClientSecret, argocdHost) => {
         json: true
     };
 
-    request(requestOptions, (error, response, body) => {
-        if (error) {
-            reject(new Error(error));
-        } else {
-            if (response.statusCode == 200) {
-                console.log(`[Info]:: session with argocd opened successfully!`);
-                resolve(body['token']);
+    return new Promise((resolve, reject) => {
+        request(requestOptions, (error, response, body) => {
+            if (error) {
+                reject(new Error(error));
             } else {
-                console.log(`[Info]:: request to open argocd session returned status: ${response.statusCode}`);
-                throw error("failed to open session in argocd")
+                if (response.statusCode == 200) {
+                    console.log(`[Info]:: session with argocd opened successfully!`);
+                    resolve(body['token']);
+                } else {
+                    console.log(`[Info]:: request to open argocd session returned status: ${response.statusCode}`);
+                    throw error("failed to open session in argocd")
+                }
             }
-        }
+        });
     });
-
-    // return new Promise((resolve, reject) => {
-    //     request(requestOptions, (error, response, body) => {
-    //         if (error) {
-    //             reject(new Error(error));
-    //         } else {
-    //             if (response.statusCode == 200) {
-    //                 console.log(`[Info]:: session with argocd opened successfully!`);
-    //                 resolve(body['token']);
-    //             } else {
-    //                 console.log(`[Info]:: request to open argocd session returned status: ${response.statusCode}`);
-    //                 throw error("failed to open session in argocd")
-    //             }
-    //         }
-    //     });
-    // });
 }
 
 // syncApplication is responsable for send request for sync trigger 
